aa91270fcbc7243f9c69545be47f878a1df6ecd2
Yossi Tokash <yossi.tokash@gmail.com>
2016-10-02 12:42:20 +0300

    Added Tools skeletons

--- XPlanner/LabElements/Tools/Tool.cs
+++ XPlanner/LabElements/Tools/Tool.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using XPlanner.LabElements.Vessels;
+using XPlanner.LabElements.Devices;
 
 namespace XPlanner.LabElements.Tools
 {
@@ -12,11 +13,91 @@ namespace XPlanner.LabElements.Tools
         Vessel m_Sample;
     }
 
-    public class Vortex: Tool
+    public enum Scale
+    {
+        Mili,
+        Micro
+    };
+
+    public class Pipette : Tool, IFlowable
+    {
+        Scale m_Scale;
+        int m_Volume;
+
+        public void Draw()
+        {
+
+        }
+        public void Release()
+        {
+
+        }
+    }
+
+    public class Vortex: Tool, IMixable
     {
         public void Mix()
         {
-            //blah
+           
         }
     }
+
+    public class Stirrer: Tool, IMixable
+    {
+        public void Mix()
+        {
+
+        }
+    }
+
+    public class Magnetic_Stirrer : Stirrer, IHeatable
+    {
+        public void Heat()
+        {
+
+        }
+    }
+
+    public class XY_Stirrer : Stirrer
+    {
+
+    }
+
+    public class PH_Meter: Tool, IMeasurable
+    {
+        public void Measure(string parameter)
+        {
+
+        }
+    }
+
+    public class Centrifuge: Tool, ISpinnable
+    {
+        public void Spin()
+        {
+
+        }
+    }
+
+    public interface ISpinnable
+    {
+        void Spin();
+    }
+
+    public interface IMixable
+    {
+        void Mix();
+    }
+
+    public interface IFlowable
+    {
+        void Draw();
+        void Release();
+    }
+
+    public interface IHeatable
+    {
+        void Heat();
+    }
+    
 }
