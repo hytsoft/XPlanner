7444b12f97c863ca679048bd6860dafe0c0b3402
hytsoft <hytsoft1@gmail.com>
2017-03-16 11:49:47 +0200

    Add Product categories uris from BI website

---
+++ LabElements/Ingredients/Ingredient.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XPlanner.LabElements.Ingredients
+{
+    // Ingredients (just like vessels) are (currently) inertic. i.e. it has no methods.
+    // Due to its divesity, this class will be defined as dynamic.
+    // Thus, structure may change constantly.
+
+    // Ingredients current structure is partly taken from 'Sigma-Aldrich' site.
+    // https://www.sigmaaldrich.com/life-science/cell-biology.html
+
+    public class Ingredient : LabElement
+    {
+        // Enums 
+
+        public enum ADME_ToxType
+        {
+            PTAs, // Permiablity and Transporter Assays
+            MAs,  // Metabolism Assays
+            TAs,  // Toxicology Assays
+        };
+
+        public enum AntibioticType
+        {
+            ABPs, // Antibiotic Products
+            ABSTs, // Antibiotic Selector Tools
+            ABs,   // Antibiotic (will be sorted by groups)
+        };
+
+        public enum AntibodyType
+        {
+           MonoclonalAntiBeta_Actin_Mouse_AC15_AF, // Monoclonal Anti-beta Actin Antibody produced in Mouse, Clone AC-15, Ascites fluid
+           AntiActin_Rabbit_AI // Anti-Actin antibody produced in Rabbit, Affinity Isolated Antibody
+        };
+
+        public enum BiochemicalType
+        {
+            NOR3, // E4Ethyl2_E_Hydroxymino_5Nitro3Hexenamide
+            NOR4, // E4Ethyl2_Z_Hydroxymino_5Nitro3Hexen1yl_Nicotinamide
+        };
+
+        public enum ReagentType
+        {
+            
+        };
+
+        public enum CellCultureType
+        {
+            Fibroblast,
+            Endothelia,
+            Epithelia,
+            Neuro,
+        };
+
+        public enum GenType
+        {
+            DNA,
+            RNA,
+            TF,
+        };
+
+        public enum BufferType
+        {
+            AMPD,
+            AMP95, 
+            ACES // N-(2-Acetamido)-2-aminoethanesulfonic acid
+        };
+
+        public enum StainType
+        {
+            AcridineOrange,
+            BismarkBrown,
+            Carmine,
+            CoomassieBlue,
+            CresylViolet,
+            CrystalViolet,
+            DAPI,
+            Eosin,
+            EthidiumBromide,
+            AcidFuchsine,
+            Hematoxilin,
+        };
+
+        // Classes - optimization may be required
+
+        public class ADME_Tox : Ingredient
+        {
+            ADME_ToxType m_type;
+        }
+
+        public class Antibody : Ingredient
+        {
+            AntibodyType m_type;
+        }
+
+        public class Biochemical : Ingredient
+        {
+            BiochemicalType m_type;
+        }
+
+        public class Reagent : Ingredient 
+        {
+            ReagentType m_type;
+        }
+
+        public class CellCulture : Ingredient
+        {
+            CellCultureType m_type;
+        }
+
+        public class Genetic : Ingredient
+        {
+            GenType m_type;
+        }
+
+        public class Buffer : Ingredient
+        {
+            BufferType m_type;
+        }
+
+        public class Stain : Ingredient
+        {
+            StainType m_type;
+        }
+
+    }
+}
---
+++ LabElements/Tools/Tool.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XPlanner.LabElements.Vessels;
+using XPlanner.LabElements.Devices;
+
+namespace XPlanner.LabElements.Tools
+{
+    public class Tool : LabElement
+    {
+        Vessel m_Sample;
+    }
+
+    public enum Scale
+    {
+        Mili,
+        Micro
+    };
+
+    public class Pipette : Tool, IFlowable
+    {
+        Scale m_Scale;
+        int m_Volume;
+
+        public void Draw()
+        {
+
+        }
+        public void Release()
+        {
+
+        }
+    }
+
+    public class Vortex: Tool, IMixable
+    {
+        public void Mix()
+        {
+           
+        }
+    }
+
+    public class Stirrer: Tool, IMixable
+    {
+        public void Mix()
+        {
+        }
+    }
+
+    public interface IHeatable
+    {
+        void Heat();
+    }
+
+    public interface ISpinnable
+    {
+        void Spin();
+    }
+
+    public interface IMixable
+    {
+        void Mix();
+    }
+
+    public interface IFlowable
+    {
+        void Draw();
+        void Release();
+    }
+}
