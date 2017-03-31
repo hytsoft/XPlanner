3edd625c17ecc0d834ec0788c194a562b25dcf46
hytsoft <hytsoft1@gmail.com>
2017-03-31 14:38:24 +0300

    GetCategories: BI Extractor

--- InternetDataGetter/InternetDataGetter/Biological_Indusries_Parser.cs
+++ InternetDataGetter/InternetDataGetter/Biological_Indusries_Parser.cs
@@ -9,6 +9,7 @@ namespace InternetDataGetter
 {
     public class BiologicalIndusriesParser : WebsiteParser
     {
+               
         public static void Parse(Uri uri)
         {
             HtmlDocument entirePage = DataGetter.GetHtmlpage(uri);
@@ -54,15 +55,111 @@ namespace InternetDataGetter
             specifications specifications = new specifications();
             List<string> elements = new List<string>();
             elements.Add(element);
-            List<KeyValuePair<string, HtmlNodeCollection>> htmlspecifications = DataGetter.GetDataByXPATH(page, elements);
-            HtmlNode Form = htmlspecifications[0].Value[0];
-            HtmlNode Storage_Conditions = htmlspecifications[0].Value[1];
-            HtmlNode Quality_Control = htmlspecifications[0].Value[2];
+            List<KeyValuePair<string, HtmlNodeCollection>> htmlSpecifications = DataGetter.GetDataByXPATH(page, elements);
+            HtmlNode Form = htmlSpecifications[0].Value[0];
+            HtmlNode storageConditions = htmlSpecifications[0].Value[1];
+            HtmlNode qualityControl = htmlSpecifications[0].Value[2];
             specifications.form = Form.InnerText; ;
-            specifications.storage_conditions = Storage_Conditions.InnerText;
-            specifications.quality_control = Quality_Control.InnerText;
+            specifications.storage_conditions = storageConditions.InnerText;
+            specifications.quality_control = qualityControl.InnerText;
             return specifications;
 
         }
+
+        public override HtmlDocument GetMainPage(Uri uri)
+        {
+            HtmlDocument mainPage = null;
+
+            if (uri != null)
+            {
+                mainPage = DataGetter.GetHtmlpage(uri);
+            }
+
+            return mainPage;
+        }
+
+        public override List<Uri> GetCategories(HtmlDocument page)
+        {
+            List<string> ids = new List<string>();
+            string id = "//a[@href]"; // define id for raw-categories retrieval
+            ids.Add(id); // add id to ids list because 'GetDataByXPATH' can only get string list
+                         
+            // allocate string lists for each category
+            List<string> str_Categories = new List<string>();
+            List<string> str_CellCulture = new List<string>();
+            List<string> str_StemCellResearch = new List<string>();
+            List<string> str_HumanCytogenetics = new List<string>();
+            List<string> str_AOI = new List<string>();
+            List<string> str_PopularBrands = new List<string>();
+            List<string> str_Mycoplasma = new List<string>();
+            List<string> str_MolecularBiology = new List<string>();
+            List<string> str_LabInstruments = new List<string>();
+
+            // search for that id in page (BI home page)
+            List<KeyValuePair<string, HtmlNodeCollection>> raw_categories = DataGetter.GetDataByXPATH(page, ids);
+
+            // extract all product-categories from HtmlDocument
+            for (int i=0; i< raw_categories[0].Value.Count(); i++)
+            {
+                
+                if (raw_categories[0].Value[i].OuterHtml.Contains("products"))
+                {
+                    string[] temp_str = raw_categories[0].Value[i].OuterHtml.Split('"');
+                    str_Categories.Add(temp_str[1]);
+                }
+            }
+
+            // divide into product-sub-categories
+            for (int i=0; i<str_Categories.Count(); i++)
+            {
+                if (str_Categories[i].Contains("cell-culture"))
+                {
+                    str_CellCulture.Add(str_Categories[i]);
+                }
+                else if (str_Categories[i].Contains("stem-cell-research"))
+                {
+                    str_StemCellResearch.Add(str_Categories[i]);
+                }
+                else if (str_Categories[i].Contains("human-cytogenetics"))
+                {
+                    str_HumanCytogenetics.Add(str_Categories[i]);
+                }
+                else if (str_Categories[i].Contains("areas-of-interest"))
+                {
+                    str_AOI.Add(str_Categories[i]);
+                }
+                else if (str_Categories[i].Contains("popular-brands"))
+                {
+                    str_PopularBrands.Add(str_Categories[i]);
+                }
+                else if (str_Categories[i].Contains("mycoplasma"))
+                {
+                    str_Mycoplasma.Add(str_Categories[i]);
+                }
+                else if (str_Categories[i].Contains("molecular-biology"))
+                {
+                    str_MolecularBiology.Add(str_Categories[i]);
+                }
+                else if (str_Categories[i].Contains("lab-instruments"))
+                {
+                    str_LabInstruments.Add(str_Categories[i]);
+                }
+            }
+            
+            List<Uri> uri = new List<Uri>();
+            return uri;
+        }
+
+        public override List<Product> GetProducts(Uri category)
+        {
+            List<Product> products = new List<Product>();
+            return products;
+        }
+
+        public override Product GetProduct(Uri product)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
--- InternetDataGetter/InternetDataGetter/Program.cs
+++ InternetDataGetter/InternetDataGetter/Program.cs
@@ -13,9 +13,18 @@ namespace InternetDataGetter
     {
         static void Main(string[] args)
         {
-            WebsiteParser BI_website_parser = new WebsiteParser();
-            List<WebsiteParser> websites = new list<WebsiteParser>;
-            websites.Add(BI_website_parser);
+            BiologicalIndusriesParser BI_website_parser = new BiologicalIndusriesParser(); //create new class instance to use abstract methods one by one
+            string BILink = "http://www.bioind.com/israel/"; //define main link to BI home page
+            Uri BI_uri = new Uri(BILink); //create uri for page retrieval
+            List<Uri> Categories = new List<Uri>(); //create list of category uris
+
+            HtmlDocument mainPage = BI_website_parser.GetMainPage(BI_uri); // get BI home page as HtmlDocument
+
+            Categories = BI_website_parser.GetCategories(mainPage); // get all category uris
+            
+            //List<WebsiteParser> websites = new list<WebsiteParser>;
+
+            //websites.Add(BI_website_parser);
         }
     }
 }
